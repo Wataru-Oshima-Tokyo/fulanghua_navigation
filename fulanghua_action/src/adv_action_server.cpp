64eca8ca5e5cb31924e1e3d742cbbb442cddefdc
Wataru <wataruoshima@my.smccd.edu>
2022-07-26 17:37:56 +0900

    added action

---
+++ fulanghua_action/src/adv_action_server.cpp
@@ -0,0 +1,53 @@
+#include <ros/ros.h>
+#include <fulanghua_action/testAction.h>
+#include <actionlib/server/simple_action_server.h>
+
+typedef actionlib::SimpleActionServer<fulanghua_action::testAction> Server;
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "task_server");
+  ros::NodeHandle nh;
+  Server server(nh, "task", false);
+  server.start();
+
+  ros::Time start_time;
+  ros::Rate loop_rate(2);
+  ros_lecture_msgs::TaskGoalConstPtr current_goal;
+  while (ros::ok())
+  {
+    if (server.isNewGoalAvailable())
+    {
+      current_goal = server.acceptNewGoal();
+      start_time = ros::Time::now();
+      printf("Update Goal\n");
+    }
+    if (server.isActive())
+    {
+      if (server.isPreemptRequested())
+      {
+        server.setPreempted();
+        printf("Preempt Goal\n");
+      }
+      else
+      {
+        if (start_time + ros::Duration(current_goal->duration) < ros::Time::now())
+        {
+          server.setSucceeded();
+          // server.setAborted();
+          printf("Active: publish result id:%i\n", current_goal->task_id);
+        }
+        else
+        {
+          ros_lecture_msgs::TaskFeedback feedback;
+          feedback.rate = (ros::Time::now() - start_time).toSec() / current_goal->duration;
+          server.publishFeedback(feedback);
+          printf("Active: publish feedback id:%i\n", current_goal->task_id);
+        }
+      }
+    }
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
