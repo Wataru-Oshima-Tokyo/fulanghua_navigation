a10655f0b0e803e42547c90ed4d8d4e8120a5765
Wataru <wataruoshima@my.smccd.edu>
2022-07-27 18:38:40 +0900

    fixed bugs

--- fulanghua_action/src/adv_action_client.cpp
+++ fulanghua_action/src/adv_action_client.cpp
@@ -48,7 +48,7 @@ int main(int argc, char** argv)
 
   int task_id = 0;
   bool initial_goal = false;
-  ros::Rate loop_rate(2);
+  ros::Rate loop_rate(20);
   while (ros::ok())
   {
     if (client.isServerConnected())
