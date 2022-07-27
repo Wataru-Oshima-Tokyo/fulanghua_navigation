2a7c86093225b8845cdb9d88e8ffbc66fe60b7c0
Wataru <wataruoshima@my.smccd.edu>
2022-07-27 17:50:09 +0900

    fixed bugs

--- fulanghua_action/src/adv_action_client.cpp
+++ fulanghua_action/src/adv_action_client.cpp
@@ -64,7 +64,7 @@ int main(int argc, char** argv)
       {
         break;
       }
-      else if(regex_match('c',regex("[a-z]"))) {
+      else if(std::regex_match('c',std::regex("[a-z]"))) {
         fulanghua_action::testGoal goal;
         goal.task_id = task_id;
         goal.command = c;
