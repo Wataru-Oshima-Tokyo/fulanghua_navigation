8736e8baf6da7314e8b025cabada8c7b5a0a7452
Wataru <wataruoshima@my.smccd.edu>
2022-07-27 18:07:20 +0900

    fixed bugs

--- fulanghua_action/src/adv_action_client.cpp
+++ fulanghua_action/src/adv_action_client.cpp
@@ -54,7 +54,7 @@ int main(int argc, char** argv)
   {
     if (client.isServerConnected())
     {
-      std::cin >> c;
+      getline (cin, c);
       if (c == "c")
       {
         client.cancelGoal();
