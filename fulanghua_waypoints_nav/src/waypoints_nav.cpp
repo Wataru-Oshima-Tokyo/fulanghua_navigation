b147a5866498ba96a861c9d10665d40b2621697f
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-07-28 09:59:54 +0900

    fixed bugs

--- fulanghua_waypoints_nav/src/waypoints_nav.cpp
+++ fulanghua_waypoints_nav/src/waypoints_nav.cpp
@@ -407,11 +407,12 @@ public:
 
    void computeWpOrientation(){
         for(std::vector<orne_waypoints_msgs::Pose>::iterator it = waypoints_.poses.begin(); it != finish_pose_; it++) {
-            double goal_direction = atan2((it+1)->position.y - (it)->position.y,
-                                          (it+1)->position.x - (it)->position.x);
-            (it)->orientation = tf::createQuaternionMsgFromYaw(goal_direction);
+            if(it->postion.action !="stop" || it->postion.action !="charge"){
+                double goal_direction = atan2((it+1)->position.y - (it)->position.y,
+                                (it+1)->position.x - (it)->position.x);
+                (it)->orientation = tf::createQuaternionMsgFromYaw(goal_direction);
+            }
             if(it->position.action=="charge"){
-                
                 charging_waypoints_.poses.push_back(*it);
                 CHARGING_STATION =true;
             }
