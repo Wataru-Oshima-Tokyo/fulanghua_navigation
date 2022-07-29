356f1160617d45f9726dbc5d3d43ccc784c995d5
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-07-29 16:28:01 +0900

    added robot pose

--- fulanghua_action/src/adv_action_server.cpp
+++ fulanghua_action/src/adv_action_server.cpp
@@ -2,11 +2,52 @@
 #include <fulanghua_action/testAction.h>
 #include <actionlib/server/simple_action_server.h>
 #include <geometry_msgs/Twist.h>
+#include <tf/tf.h>
+#include <tf/transform_listener.h>
 
 
-void chargingFunction(){
-    printf("charging action here\n");
+class SpecialMove{
+  public:
+
+
+    SpecialMove(){
+          private_nh.param("cmd_vel_posture", cmd_vel_, std::string("cmd_vel_posture_"));
+    }
+    void chargingFunction(){
+          printf("charging action here\n");
+    }
+
+    bool onNavigationPoint(const orne_waypoints_msgs::Waypoint &dest, double dist_err = 0.8){
+        tf::StampedTransform robot_gl = getRobotPosGL();
+
+        const double wx = dest.x;
+        const double wy = dest.y;
+        const double rx = robot_gl.getOrigin().x();
+        const double ry = robot_gl.getOrigin().y();
+        const double dist = std::sqrt(std::pow(wx - rx, 2) + std::pow(wy - ry, 2));
+
+        return dist < dist_err;
+    }
+
+    tf::StampedTransform getRobotPosGL(){
+        tf::StampedTransform robot_gl;
+        try{
+            tf_listener_.lookupTransform(world_frame_, robot_frame_, ros::Time(0.0), robot_gl);
+        }catch(tf::TransformException &e){
+            ROS_WARN_STREAM("tf::TransformException: " << e.what());
+        }
+
+        return robot_gl;
+    }
+
+    ros::NodeHandle nh;
+    ros::NodeHandle private_nh("~");
+    std::string cmd_vel_;
+    ros::Publisher twist_pub;
+    twist_pub = nh.advertise<geometry_msgs::Twist>(cmd_vel_,1000);
 }
+
+
 
 
 typedef actionlib::SimpleActionServer<fulanghua_action::testAction> Server;
@@ -14,14 +55,11 @@ typedef actionlib::SimpleActionServer<fulanghua_action::testAction> Server;
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "task_server");
-  ros::NodeHandle nh;
-  ros::NodeHandle private_nh("~");
-  std::string cmd_vel_;
-  private_nh.param("cmd_vel_posture", cmd_vel_, std::string("cmd_vel_posture_"));
+  SpecialMove SpM;
   Server server(nh, "action", false);
   server.start();
-  ros::Publisher twist_pub;
-  twist_pub = nh.advertise<geometry_msgs::Twist>(cmd_vel_,1000);
+  
+  
   ros::Time start_time;
   ros::Rate loop_rate(20);
   fulanghua_action::testGoalConstPtr current_goal;
