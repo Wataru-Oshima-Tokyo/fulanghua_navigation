54c52bf50871187e89d729824d974acd602acb66
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-07-29 17:47:24 +0900

    added p2p

--- fulanghua_action/src/adv_action_server.cpp
+++ fulanghua_action/src/adv_action_server.cpp
@@ -12,8 +12,10 @@ class SpecialMove{
 
     SpecialMove(){
           ros::NodeHandle private_nh("~");
-          private_nh.param("cmd_vel_posture", cmd_vel_, std::string("cmd_vel_posture_"));
+          private_nh.param("cmd_vel_posture", cmd_vel_posture, std::string("cmd_vel_posture_"));
+          private_nh.param("cmd_vel_", cmd_vel_posture, std::string("cmd_vel"));
           private_nh.param("dist_err", _dist_err, std::string("0.8"));
+          server.start();
     }
 
     void coordinate_callback(const geometry_msgs::Point& point){
@@ -25,7 +27,14 @@ class SpecialMove{
           printf("charging action here\n");
     }
 
-
+    void P2P_move(){
+      if (!onNavigationPoint){
+          twist.linear.x = 1;
+          twist_move_pub.publish(twist);
+      }else{
+          server.setPreempted();
+      }
+    }
 
     bool onNavigationPoint(const orne_waypoints_msgs::Waypoint &dest, double dist_err = 0.8){
         const double wx = dest.x;
@@ -37,9 +46,11 @@ class SpecialMove{
 
     ros::NodeHandle nh;
     tf::TransformListener tf_listener_;
-    std::string cmd_vel_, _dist_err;
-    ros::Publisher twist_pub; 
+    std::string cmd_vel_, _dist_err,cmd_vel_posture;
+    ros::Publisher twist_move_pub, twist_postgure_pub; 
     ros::Subscriber robot_coordinate_sub;
+    geometry_msgs::Twist twist;
+    Server server(SpM.nh, "action", false);
     double rx, ry;
 };
 
@@ -51,26 +62,25 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "task_server");
   SpecialMove SpM;
-  Server server(SpM.nh, "action", false);
-  server.start();
   ros::Time start_time;
   ros::Rate loop_rate(20);
-  SpM.twist_pub = SpM.nh.advertise<geometry_msgs::Twist>(SpM.cmd_vel_,1000);
+  SpM.twist_postgure_pub = SpM.nh.advertise<geometry_msgs::Twist>(SpM.cmd_vel_posture,1000);
+  SpM.twist_move_pub = SpM.nh.advertise<geometry_msgs::Twist>(SpM.cmd_vel_,1000);
   SpM.robot_coordinate_sub = SpM.nh.subscribe("robot_coordniate", 1000, &SpecialMove::coordinate_callback, &SpM);
   fulanghua_action::testGoalConstPtr current_goal;
   while (ros::ok())
   {
-    if (server.isNewGoalAvailable())
+    if (SpM.server.isNewGoalAvailable())
     {
-      current_goal = server.acceptNewGoal();
+      current_goal = SpM.server.acceptNewGoal();
       start_time = ros::Time::now();
       printf("Update Goal\n");
     }
-    if (server.isActive())
+    if (SpM.server.isActive())
     {
-      if (server.isPreemptRequested())
+      if (SpM.server.isPreemptRequested())
       {
-        server.setPreempted();
+        SpM.server.setPreempted();
         printf("Preempt Goal\n");
       }
       else
@@ -78,7 +88,7 @@ int main(int argc, char** argv)
         geometry_msgs::Twist twist;
         if (start_time + ros::Duration(current_goal->duration) < ros::Time::now())
         {
-          server.setSucceeded();
+          SpM.server.setSucceeded();
           // server.setAborted();
           
         }
@@ -86,7 +96,7 @@ int main(int argc, char** argv)
         {
           fulanghua_action::testFeedback feedback;
           feedback.rate = (ros::Time::now() - start_time).toSec() / current_goal->duration;
-          server.publishFeedback(feedback);
+          SpM.server.publishFeedback(feedback);
           // printf("Active: publish feedback id:%i\n", current_goal->task_id);
           std::cout << "received command: " << current_goal->command <<std::endl;
           // printf("Active: publish result id:%i\n", current_goal->task_id);
@@ -113,6 +123,9 @@ int main(int argc, char** argv)
           }
           else if (current_goal->command =="charge"){
             SpM.chargingFunction();  
+          }
+          else if (current_goal->command =="p2p"){
+            SpM.P2P_move();  
           }
           else if (current_goal->command =="takephoto"){
              printf("take photo\n");
