e279571d91d6da410d71da35af4a54ff450cd4c8
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-08-02 09:38:30 +0900

    added an angle calculation

--- fulanghua_action/src/adv_action_server.cpp
+++ fulanghua_action/src/adv_action_server.cpp
@@ -53,6 +53,7 @@ class SpecialMove{
         twist.linear.x = 0.1;
         printf("cmd_vel_x = %f\n", velocity_x);
         printf("dist = %f\n", dist);
+        printf("wr-ry = %f\n", std::abs(wy-ry));
         printf("angle = %f\n", angle);
         return dist < dist_err;
     }
