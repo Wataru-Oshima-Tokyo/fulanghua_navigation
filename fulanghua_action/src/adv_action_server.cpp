f73f01fb6be5cfafb8a70580445d86bc028d5a8e
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-08-03 10:59:29 +0900

    changed Kp kv

--- fulanghua_action/src/adv_action_server.cpp
+++ fulanghua_action/src/adv_action_server.cpp
@@ -126,7 +126,7 @@ class SpecialMove{
     const double hz =20;
   private:
     const double Kp = 0.5;
-    const double Kv = 0.4775;
+    const double Kv = 0.2865;
     orne_waypoints_msgs::Pose direction;
     double velocity_x;
     double rx, ry;
